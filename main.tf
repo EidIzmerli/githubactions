6f34010403d98fa45f841dd7ed0dd95ff2751bb3
Eid Alizmerli <129078497+EidIzmerli@users.noreply.github.com>
2025-05-19 11:05:25 +0000

    delete unneeded file

--- main.tf
+++ main.tf
@@ -2,13 +2,6 @@
     The configuration for the `remote` backend.
      terraform {
        backend "local" {
-          The name of your Terraform Cloud organization.
-         organization = "example-organization"
-
-          The name of the Terraform Cloud workspace to store Terraform state files in.
-         workspaces {
-           name = "example-workspace"
-         }
        }
      }
 
